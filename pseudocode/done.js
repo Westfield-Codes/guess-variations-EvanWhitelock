d76f385cf6012c20771a9d838b38e2f6f4e02da1
EvanWhitelock <evan202503@student-schoolsofwestfield.org>
2024-10-08 13:17:46 +0000

    guess loop quit

--- pseudocode/done.js
+++ pseudocode/done.js
@@ -1,50 +1,28 @@
-/* Define a function called rollDice */
-function rollDice(){
-    // create a variable called roll, set it equal to random 1-6
-    let roll = Math.floor(Math.random()*6+1);
-    // if roll equals 6, display "6! You win!"
-  if (roll == 6) alert("6! You win!");
-    // otherwise:
-  else{
-    // use a confirm and say: "You rolled a [roll]"
-  confirm("You rolled a " + roll);
-    // then call rollDice again.
-  rollDice();
-    // close the else block
-  }
-  // close the function block
-  }
+/* 3.guess-loop-quit.js */
 
-
-/* Simple Guess Program */
-
-// create global variable guess, set to 0
-var guess = 0;
-// create a global variable called answer and store a random integer 1-100
-let answer = Math.floor(Math.random()*100+1);
 // define function guessGame
-function guessGame(){
-// display the answer for testing purposes only
+function guessGame() {
+  // generate a random integer 1-100
+let answer = Math.floor(Math.random()*100+1);
+  // display the integer for testing purposes only
 alert(answer);
-// set guess equal to prompt asking user to "Guess a number, 1-100"
+  // create guess and set equal to 0 
+var guess = 0;
+  // loop while guess is not equal to answer or to "q"
+while (guess != answer || guess == "q") {
+    // set guess equal to asking user to "Guess a number, 1-100"
 guess = prompt("Guess a number, 1-100");
-// if guess equals answer display "Correct!" 
-if (guess == answer) alert("Correct!");
-// or if guess is greater than answer create a code block
-else if (guess > answer){
-// display "Too high!" 
-alert("Too high!");
-// call guessGame
-guessGame();
-// close the else code block
-}
-// otherwise open a code block
-else{
-// display "Too low!" 
-alert("Too low!");
-// call guessGame
-guessGame();
-// close the else code block
+    // if guess equals "q" display "Quitter!"
+if (guess == "q") alert("Quitter!");
+    // or if guess equals answer display "Correct!" 
+else if (guess == answer) alert("Correct!");
+    // or if guess is greater than answer display "Too high!" 
+else if (guess > answer) alert("Too high!");
+    // or if guess is less than answer display "Too low!" 
+else if (guess < answer) alert("Too low!");
+    // or say "Bad input"
+else alert("Bad input");
+  // end the loop
 }
 // end the function
 }
--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-/* Simple Guess Program */
+/* 3.guess-loop-quit.js */
 
 // define function guessGame
 function guessGame() {
   // generate a random integer 1-100
-  var answer = Math.floor(Math.random()*100+1);
+let answer = Math.floor(Math.random()*100+1);
   // display the integer for testing purposes only
 alert(answer);
   // create guess and set equal to 0 
-var guess = 0
-  // loop while guess is not equal to answer
-while (guess != answer) {
+var guess = 0;
+  // loop while guess is not equal to answer or to "q"
+while (guess != answer || guess == "q") {
     // set guess equal to asking user to "Guess a number, 1-100"
 guess = prompt("Guess a number, 1-100");
-    // if guess equals answer display "Correct!" 
-if (guess == answer) alert("Correct!");
+    // if guess equals "q" display "Quitter!"
+if (guess == "q") alert("Quitter!");
+    // or if guess equals answer display "Correct!" 
+else if (guess == answer) alert("Correct!");
     // or if guess is greater than answer display "Too high!" 
 else if (guess > answer) alert("Too high!");
     // or if guess is less than answer display "Too low!" 
-else if (guess < answer) alert
-    // else say "Bad input"
-
+else if (guess < answer) alert("Too low!");
+    // or say "Bad input"
+else alert("Bad input");
   // end the loop
 }
 // end the function
